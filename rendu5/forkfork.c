33c420e9b2fd73ce7399f8178886e849a242d8bd
halex02 <halex02@msn.com>
2015-10-15 23:27:53 +0200

    un commit du forkfork en l'honneur de sa sainteté Raptor Jésus

--- rendu5/forkfork.c
+++ rendu5/forkfork.c
@@ -1,1 +1,33 @@
 #include "forkfork.h"
+
+void recfork (func_t f, void *arg, int *acc) {
+
+  if (*acc == RECLEVEL-1){
+    f(arg) ;
+    return ;
+  }
+  
+  switch(fork()){
+
+  case -1 :
+    perror("fork : boom\n") ;
+    exit(EXIT_FAILURE) ;
+
+  case 0 :
+    ++(*acc);
+    recfork(f, arg, acc) ;
+    exit(EXIT_SUCCESS) ;
+
+  case 1 :
+    return ;
+  }
+    
+}
+
+void forkfork (func_t f, void *arg){
+  int acc ;
+
+  recfork(f, arg, &acc) ;
+
+  return ;
+}
